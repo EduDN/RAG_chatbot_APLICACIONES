e2b95b5b4326005208d1fbb823908293124071ff
Eduardo Domínguez <87111846+EduDN@users.noreply.github.com>
2024-05-08 18:10:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ load_dotenv()
 
 # Configure the Llama index settings
 Settings.llm = HuggingFaceInferenceAPI(
-    model_name="model_name_in_spanish",
-    tokenizer_name="tokenizer_name_in_spanish",
+    model_name="google/gemma-1.1-7b-it",
+    tokenizer_name="google/gemma-1.1-7b-it",
     context_window=3000,
     token=os.getenv("HF_TOKEN"),
     max_new_tokens=512,
     generate_kwargs={"temperature": 0.1},
 )
 Settings.embed_model = HuggingFaceEmbedding(
-    model_name="model_name_in_spanish"
+    model_name="BAAI/bge-small-en-v1.5"
 )
 
 # Define the directory for persistent storage and data
